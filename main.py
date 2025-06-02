22496f3e10b68a96cca73d87d88fd302e6ccae98
Siavash Kiamousavi <siavashkiamousavi@gmail.com>
2025-06-02 12:20:00 +0330

    Subtraction Function was deleted and Divider Function was added!

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def multiplier(a, b):
 def addition(a, b):
     return a + b
 
-def subtraction(a, b):
-    return a - b
-
+def divider(a, b):
+    if b == 0:
+        raise ValueError("Cannot divide by zero")
+    return a / b
